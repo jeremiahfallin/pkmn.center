09d6defdeafa30ebf54a42adae75430f8b9391f8
Jeremiah <jeremiah.fallin@gmail.com>
2023-01-06 11:11:41 -0800

    first

--- src/components/PokeRate.tsx
+++ src/components/PokeRate.tsx
@@ -10,15 +10,15 @@ import { trpc } from 'utils/trpc';
 export default function PokeRate(pokemon: any) {
   const { data: session } = useSession();
   const [rating, setRating] = useState(pokemon.rate);
-  const mutation = trpc.pokemon.upsert.useMutation();
+  // const mutation = trpc.pokemon.upsert.useMutation();
 
-  const leaveRate = async (id: number, rate: number) => {
-    setRating(rate);
-    mutation.mutate({
-      pokemonId: id,
-      rate,
-    });
-  };
+  // const leaveRate = async (id: number, rate: number) => {
+  //   setRating(rate);
+  //   mutation.mutate({
+  //     pokemonId: id,
+  //     rate,
+  //   });
+  // };
 
   return (
     <>
@@ -28,7 +28,7 @@ export default function PokeRate(pokemon: any) {
           <Rate
             count={5}
             value={rating}
-            onChange={(rate) => leaveRate(pokemon.id, rate)}
+            // onChange={(rate) => leaveRate(pokemon.id, rate)}
           />
         </Box>
       )}
